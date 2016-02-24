899274d0d1524bb87bc1091ee1aa4c0e5610154c
yoyoonarock <yoyoonarock@gmail.com>
2016-02-24 04:08:46 -0600

    first commit of reddit scraper
    The script selects the top (n) posts in a certain subreddit, and
    calculates the user's karma score for all of his/her comments in those
    posts

---
+++ datacollection.py
@@ -0,0 +1,55 @@
+import praw
+import json
+
+
+test_limit = 1 
+r = praw.Reddit(user_agent='Test Script CS 467')
+
+subreddit = r.get_subreddit('python')
+submissions = subreddit.get_top_from_all(limit=test_limit)
+
+uniqueusers = []
+d = {}
+f = open('users.txt', 'w')
+
+for submission in submissions:
+	submission.replace_more_comments(limit=64, threshold=10)
+	flat_comments = praw.helpers.flatten_tree(submission.comments)
+	for comment in flat_comments:
+		if comment.author == None:
+			continue
+		user = comment.author.name
+		if user not in uniqueusers:
+			uniqueusers.append(user)
+			d[user] = 0
+		d[user] += comment.score
+		
+#	for i in comments:
+#		if i.author not in uniqueusers:
+#			uniqueusers.append(i)
+
+
+for u in sorted(d, key=d.get, reverse=True):
+	f.write(u)
+	f.write(' ')
+	f.write(str(d[u]))
+	f.write("\n")
+
+
+
+
+
+
+
+
+'''
+for u in uniqueusers:
+	f.write(u)
+	f.write(' ')
+	f.write(str(d[u]))
+	f.write("\n")
+
+'''
+
+
+#f.write(comment.body.encode('utf-8'))
